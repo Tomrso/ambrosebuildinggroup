2573961c4cb66c350a7b820e04c2cd3b00c62277
Tom <thomas.wilson98@hotmail.com>
2026-03-06 16:53:42 +1100

    just needs pics & proofing

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,22 +1,21 @@
 module.exports = {
-    name: "Tomic Painting & Decorating",
-    email: "info@tomicpainting.com",
-    phoneForTel: "0449200343",
-    phoneFormatted: "0449 200 343",
+    name: "Ambrose Building Group",
+    email: "ambrosebuildinggroup.com.au",
+    phoneForTel: "0401218243",
+    phoneFormatted: "0401 218 243",
     address: {
-        lineOne: "",
+        lineOne: "14A, 21 Cook Road",
         lineTwo: "",
-        city: "Melbourne",
+        city: "Mitcham",
         state: "Victoria",
-        zip: "3000",
-        mapLink: "https://www.google.com/maps/place/Tomic+Painting+Decorating/@-38.0037631,144.3937313,9z/data=!3m1!4b1!4m6!3m5!1s0x6ad61b1b35b1e267:0x27eb1e9c0207bbb5!8m2!3d-38.005604!4d145.0531353!16s%2Fg%2F11nnnr4y5b?entry=ttu&g_ep=EgoyMDI2MDMwMi4wIKXMDSoASAFQAw%3D%3D",
+        zip: "3132",
+        mapLink: "https://www.google.com/maps/place/Ambrose+Building+Group+Pty+Ltd/@-37.7952847,144.8709663,10z/data=!3m1!4b1!4m6!3m5!1s0x2138ffaa2693afa1:0x6aeaed76118910e7!8m2!3d-37.7957439!4d145.2006126!16s%2Fg%2F11ydwh3tgr?entry=ttu&g_ep=EgoyMDI2MDMwMi4wIKXMDSoASAFQAw%3D%3D",
     },
     socials: {
-        facebook: "https://www.facebook.com/tomicpainting/",
-        instagram: "https://www.instagram.com/tomicpainting_decorating",
+        instagram: "https://www.instagram.com/ambrose_building_group/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://tomicpainting.com",
+    domain: "https://ambrose-buildinggroup.com.au",
     // Passing the isProduction variable for use in HTML templates
     isProduction: process.env.ELEVENTY_ENV === "PROD",
 };
